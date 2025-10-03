1c517419998b05b72770a3b9ef29a73e33dee8ef
zottrygroups <zottrygroups@gmail.com>
2025-10-04 01:16:25 +0530

    Fix CRA deployment: homepage, rewrites, router placement

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
@@ -14,7 +15,9 @@ root.render(
       <AuthProvider>
         <WalletProvider>
           <LotteryDataProvider>
-            <App />
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
           </LotteryDataProvider>
         </WalletProvider>
       </AuthProvider>
